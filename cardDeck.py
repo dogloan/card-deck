6f9567bf896956ad93d8ddba9dada4436339eb94
dogloan <99242417+dogloan@users.noreply.github.com>
2022-07-22 17:58:20 -0400

    Update cardDeck.py

--- cardDeck.py
+++ cardDeck.py
@@ -1,5 +1,13 @@
 import random
 
+
+"""
+TO DO seperate pulling a card and adding cards to hands into seperate functions
+TO DO seperate deck and dealer functions
+TO DO currently the number of players dealt to card_puller can't exceed player amount
+TO DO given to CardDeck
+"""
+
 class CardDeck:
 # the CardDeck class will be used to create and track the deck, deal and remember
 # cards, and basically do any other card related function or variable tracking
@@ -13,7 +21,6 @@ class CardDeck:
         # self.hand will be a list of lists, each list being a players hand containing
         # a list of their cards
         self.hand = []
-        self.players = players
 
         # create a hand for each player and add it to the master list of hands
         for n in range(players):
@@ -51,7 +58,7 @@ class CardDeck:
     # the purpose of the card puller is to pull a card from the deck,
     # add it to the hand, and remove the pulled card from the remaining deck
     # so that it cannot be pulled again until the deck is reset
-    def card_puller(self, players = 1, cards = 1):
+    def card_puller(self, cards, players = 1):
 
         # card_puller will pull as many cards as prompted or pull just one
         # if not called with an integer
@@ -66,3 +73,57 @@ class CardDeck:
                 self.hand[n].append(card)
                 # remove chosen card from decks
                 self.deck.remove(card)
+
+    # shuffle the deck, not to be confused with reseting it
+    def deck_shuffler(self):
+        random.shuffle(self.deck)
+
+    # translator the card information into normal english
+    def card_translator(self, card):
+        # this will be the new values
+        face_value = ""
+        suit_value = ""
+
+        # lets figure out the face value of the card
+        if card[0] in range(10):
+            face_value = str(card[0])
+        elif card[0] == 10:
+            face_value = "jack"
+        elif card[0] == 11:
+            face_value = "queen"
+        elif card[0] == 12:
+            face_value = "king"
+        else:
+            face_value = "ace"
+
+        # lets figure out the suit of the card
+        if card[1] == "h":
+            suit_value = "hearts"
+        elif card[1] == "d":
+            suit_value = "diamonds"
+        elif card[1] == "c":
+            suit_value = "clubs"
+        else:
+            suit_value = "spades"
+
+        # make a new string of our new card and return that value
+        translated_card = "".join([face_value, " of ", suit_value])
+        return translated_card
+
+
+    # print hands
+    """
+    TO DO see_hands is still under construction
+    """
+    def see_hands(self):
+        # everytime we count out a new players hand we'll add them to our
+        # player count
+        player_count = 0
+
+        # read out hands and count players
+        for n in self.hand:
+            player_count += 1
+            player_hand = n
+            player_statement = "".join(["Player ", str(player_count), " has:"])
+            print(player_statement)
+            print(len(player_hand))
